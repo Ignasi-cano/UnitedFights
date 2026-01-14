3fa2cea49d2790d3629499fd4c51e79334764615
Ignasi-cano <ignasi1304@gmail.com>
2026-01-14 01:43:21 +0100

    HelpUI

--- Assets/_Scripts/UI/HelpUI.cs
+++ Assets/_Scripts/UI/HelpUI.cs
@@ -2,15 +2,26 @@ using UnityEngine;
 
 public class HelpUI : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("UI References")]
+    [Tooltip("Drag the 'HelpIMG' GameObject here from the Hierarchy.")]
+    public GameObject helpPanel;
 
-    // Update is called once per frame
-    void Update()
+    // This public method will be called by the button's OnClick event
+    public void OpenClick()
     {
-        
+        // Check if the variable has been assigned in the Inspector
+        if (helpPanel != null)
+        {
+            // Make the HelpIMG GameObject visible
+            helpPanel.SetActive(true);
+            
+            // Optional: If you want the button to toggle the help window on and off, 
+            // use this line instead of the one above:
+            // helpPanel.SetActive(!helpPanel.activeSelf);
+        }
+        else
+        {
+            Debug.LogError("HelpIMG has not been assigned in the inspector!");
+        }
     }
 }
