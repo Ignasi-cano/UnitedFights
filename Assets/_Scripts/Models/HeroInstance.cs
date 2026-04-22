5af703cdcf436587007cf0f939fdf16b0cd2a47d
FoxThor05 <foxthor2005@gmail.com>
2026-04-22 11:58:20 +0200

    Modificar Aspecto Cartas

--- Assets/_Scripts/Models/HeroInstance.cs
+++ Assets/_Scripts/Models/HeroInstance.cs
@@ -9,15 +9,58 @@ public class HeroInstance
     public int MaxHealthBonus { get; set; }
     public SlotPosition Position { get; set; }
 
+    // 1 = base obtenida, 2 = una copia extra, 3 = evoluciona
+    public int EvolutionCopies { get; private set; } = 1;
+
     public HeroInstance(HeroData data)
     {
         Data = data;
         CurrentHealth = data.Health;
         MaxHealthBonus = 0;
+        Position = SlotPosition.Frontline;
+        EvolutionCopies = 1;
     }
 
     public int GetMaxHealth()
     {
         return Data.Health + MaxHealthBonus;
+    }
+
+    public void AddCopy()
+    {
+        EvolutionCopies++;
+    }
+
+    public void ResetCopies()
+    {
+        EvolutionCopies = 1;
+    }
+
+    public void EvolveTo(HeroData evolvedData, bool preserveHealthPercent = true)
+    {
+        if (evolvedData == null) return;
+
+        float healthPercent = GetMaxHealth() > 0 ? (float)CurrentHealth / GetMaxHealth() : 1f;
+        SlotPosition currentPosition = Position;
+        int currentBonus = MaxHealthBonus;
+
+        Data = evolvedData;
+        MaxHealthBonus = currentBonus;
+        Position = currentPosition;
+
+        if (preserveHealthPercent)
+        {
+            CurrentHealth = Mathf.Clamp(
+                Mathf.RoundToInt(GetMaxHealth() * healthPercent),
+                1,
+                GetMaxHealth()
+            );
+        }
+        else
+        {
+            CurrentHealth = GetMaxHealth();
+        }
+
+        ResetCopies();
     }
 }
